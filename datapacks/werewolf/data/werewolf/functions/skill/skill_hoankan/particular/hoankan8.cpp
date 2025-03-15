1e83ca3fd5893e14d9d89e586574159941d4c165
sakitibi <sakitibi10nin@icloud.com>
2025-03-16 07:15:48 +0900

    Create hoankan8.cpp

---
+++ datapacks/werewolf/data/werewolf/functions/skill/skill_hoankan/particular/hoankan8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+void processSkillHoankan8() {
+    std::cout << "Executing skill hoankan 8" << std::endl;
+}
+
+int main() {
+    processSkillHoankan8();
+    return 0;
+}
