c5923451bcd10f831d26c71e7a6b2665c37051be
sakitibi <sakitibi10nin@icloud.com>
2025-03-16 09:03:06 +0900

    Update death.cpp

--- datapacks/werewolf/data/werewolf/functions/skill/skill_cupid/death.cpp
+++ datapacks/werewolf/data/werewolf/functions/skill/skill_cupid/death.cpp
@@ -18,10 +18,10 @@ void killCupid() {
 }
 
 void processDeath() {
-    // 亡き思い人の後を追い、あなたは永遠の愛へと旅立った…
+    "亡き思い人の後を追い、あなたは永遠の愛へと旅立った…"
     tellLoversDeathMessage();
 
-    // 亡き思い人達の後を追い、あなたは永遠の愛へと旅立った…
+    "亡き思い人達の後を追い、あなたは永遠の愛へと旅立った…"
     tellCupidDeathMessage();
 
     // Kill lovers
