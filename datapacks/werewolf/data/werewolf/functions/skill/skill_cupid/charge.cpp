4207e1ea40e6532fa3a942895b46a29b2bd45f79
sakitibi <sakitibi10nin@icloud.com>
2025-03-16 09:01:07 +0900

    Update charge.cpp

--- datapacks/werewolf/data/werewolf/functions/skill/skill_cupid/charge.cpp
+++ datapacks/werewolf/data/werewolf/functions/skill/skill_cupid/charge.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cooltime/setup.hpp>
+#include <cooltime/set_cooltime.hpp>
 
 void addChargeCupidArrowScore() {
     std::cout << "Adding charge_cupid_arrow score" << std::endl;
