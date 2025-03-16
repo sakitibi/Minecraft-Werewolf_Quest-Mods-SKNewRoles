97c78fd3f4d53411c12be322462cf1dc0b31f34c
sakitibi <sakitibi10nin@icloud.com>
2025-03-16 09:00:05 +0900

    Update damage.cpp

--- datapacks/werewolf/data/werewolf/functions/skill/skill_cupid/damage.cpp
+++ datapacks/werewolf/data/werewolf/functions/skill/skill_cupid/damage.cpp
@@ -1,36 +1,65 @@
 #include <iostream>
 #include <string>
 
-void executeCommand(const std::string& command) {
-    std::cout << command << std::endl;
+void removeHitCupidArrowTag() {
+    std::cout << "Removing hit_cupid_arrow tag" << std::endl;
+}
+
+void addLoversTag() {
+    std::cout << "Adding lovers tag" << std::endl;
+}
+
+void addLovers2Tag() {
+    std::cout << "Adding lovers_2 tag" << std::endl;
+}
+
+void notifyPotentialLover() {
+    std::cout << "Notifying potential lover" << std::endl;
+}
+
+void addLovers1Tag() {
+    std::cout << "Adding lovers_1 tag" << std::endl;
+}
+
+void announceLovers() {
+    std::cout << "Announcing lovers" << std::endl;
+}
+
+void playLoveSound() {
+    std::cout << "Playing love sound" << std::endl;
+}
+
+void displayHeartParticles() {
+    std::cout << "Displaying heart particles" << std::endl;
+}
+
+void removeIncompleteLoversTags() {
+    std::cout << "Removing incomplete lovers tags" << std::endl;
 }
 
 void processDamage() {
     // ヒットタグを削除
-    executeCommand("tag @s remove hit_cupid_arrow");
+    removeHitCupidArrowTag();
 
     // タグ処理
-    executeCommand("tag @s[type=player] add lovers");
-
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] unless entity @a[tag=player,tag=lovers_2] run tag @s add lovers_2");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_2] unless entity @a[tag=player,tag=lovers_1] run tellraw @a[team=Cupid] [{\"selector\":\"@s\",\"color\":\"light_purple\"},{\"text\":\"は誰かの恋人になりそうだ。\",\"color\":\"light_purple\"}]");
-
-    executeCommand("execute unless entity @a[tag=player,tag=lovers_1] run tag @s add lovers_1");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] unless entity @a[tag=player,tag=lovers_2] run tellraw @a[team=Cupid] [{\"selector\":\"@s\",\"color\":\"light_purple\"},{\"text\":\"は誰かの恋人になりそうだ。\",\"color\":\"light_purple\"}]");
-
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] run tellraw @a[team=Cupid] [{\"selector\":\"@a[tag=lovers_1]\",\"color\":\"light_purple\"},{\"text\":\"と\",\"color\":\"light_purple\"},{\"selector\":\"@a[tag=lovers_2]\",\"color\":\"light_purple\"},{\"text\":\"は恋人となった。\",\"color\":\"light_purple\"}]");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] run tellraw @a[tag=lovers] [{\"selector\":\"@a[tag=lovers_1]\",\"color\":\"light_purple\"},{\"text\":\"と\",\"color\":\"light_purple\"},{\"selector\":\"@a[tag=lovers_2]\",\"color\":\"light_purple\"},{\"text\":\"は恋人となった。\",\"color\":\"light_purple\"}]");
+    addLoversTag();
+    addLovers2Tag();
+    notifyPotentialLover();
+    addLovers1Tag();
+    notifyPotentialLover();
+    announceLovers();
+    announceLovers();
 
     // 演出等
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] as @a[team=Cupid] at @s run playsound minecraft:fall_in_love master @s");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] as @a[tag=player,tag=lovers] at @s run playsound minecraft:fall_in_love master @s");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] at @a[tag=lovers] run particle minecraft:heart ~ ~1.7 ~ 0.2 0.2 0.2 0 5 force @a[team=Cupid]");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] at @a[tag=lovers] run particle minecraft:heart ~ ~1.7 ~ 0.2 0.2 0.2 0 5 force @a[tag=lovers]");
+    playLoveSound();
+    playLoveSound();
+    displayHeartParticles();
+    displayHeartParticles();
 
     // カップル完成時にカップル未完成のラバーズタグを削除
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] run tag @a[tag=!player] remove lovers");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] run tag @a[tag=!player] remove lovers_1");
-    executeCommand("execute if entity @a[tag=player,tag=lovers_1] if entity @a[tag=player,tag=lovers_2] run tag @a[tag=!player] remove lovers_2");
+    removeIncompleteLoversTags();
+    removeIncompleteLoversTags();
+    removeIncompleteLoversTags();
 }
 
 int main() {
