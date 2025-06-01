efda2b9f956f23170cd23d135e3d0c17f921cda6
さきちび <sakitibi10nin@icloud.com>
2025-06-01 16:53:56 +0900

    Update add_text.hpp

--- Minecraft-Werewolf-Mod-SKNewRoles-main/datapacks/Seka_C++Modules/data/12ninadds/functions/add_text.hpp
+++ Minecraft-Werewolf-Mod-SKNewRoles-main/datapacks/Seka_C++Modules/data/12ninadds/functions/add_text.hpp
@@ -1,71 +1,139 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <map>
 
-struct Message {
-    int trigger;
-    std::string text;
-    std::string team;
-    std::string color;
-    std::string url;
-    bool clickable = false;
-};
-
-class AddTextManager {
-public:
-    int add_random = 0;
-    std::string team = "";
-    std::map<std::string, int> tags;
-    int satsumatoimo_role = 0;
-    bool witch_weakness = false;
-
-    void show_message(const Message& msg) {
-        std::cout << "[Trigger: " << msg.trigger << "] ";
-        if (!msg.team.empty()) std::cout << "[Team: " << msg.team << "] ";
-        if (!msg.color.empty()) std::cout << "[Color: " << msg.color << "] ";
-        std::cout << msg.text;
-        if (msg.clickable && !msg.url.empty()) std::cout << " (URL: " << msg.url << ")";
-        std::cout << std::endl;
-    }
-
-    void process() {
-        std::vector<Message> messages = {
-            {0, "名前は長い方が有利を知っていますか? 名前は長い方が有利は悪質な荒らしです！\n 詳しくはこちらのリンクから https://sakitibi-com9.webnode.jp/page/25/", "!Fusanka", "", "", false},
-            {0, "名前は長い方が有利を知っていますか? 名前は長い方が有利は悪質な荒らしです！\n 詳しくはこちらのリンクから", "Fusanka", "", "https://sakitibi-com9.webnode.jp/page/25/", true},
-            {100, "フレンドコード：ourluckシャープ4319 を見かけた際は 今すぐ \nsupport@innersloth.com へお問い合わせして下さい、皆様のご協力をお願い致します。", "!Fusanka", "", "", false},
-            {100, "フレンドコード：ourluckシャープ4319 を見かけた際は 今すぐ \nsupport@innersloth.com へお問い合わせして下さい、皆様のご協力をお願い致します。", "Fusanka", "", "mailto:support@innersloth.com", true},
-            {200, "名前は長い方が有利はYoutubeで登録者を購入しています!今すぐ通報をして下さい!", "", "", "", false},
-            {300, "熟成牛タンは非常に悪質なページ荒らしです! もし見かけた際は \nhttps://sakitibi-com9.webnode.jp/page/3/ までお問い合わせ下さい。", "!Fusanka", "", "", false},
-            {300, "熟成牛タンは非常に悪質なページ荒らしです! もし見かけた際は \nhttps://sakitibi-com9.webnode.jp/page/3/ までお問い合わせ下さい。", "Fusanka", "", "https://sakitibi-com9.webnode.jp/page/3/", true},
-            {400, "たこたこ焼きは管理者権限の乱用をしています。絶対にマイ鉄ネットから離れて下さい。", "", "", "", false},
-            {500, "Minecraft総合交通ネットワーク は危険なサイトです 絶対に開かないで下さい。", "", "", "", false},
-            {600, "名前は長い方が有利の被害者が50人以上います。名前は長い方が有利のメンバーシップを今すぐ解約して下さい。", "", "", "", false},
-            {700, "名前は長い方が有利のライブの概要欄はどれだけ言われようが絶対に見ないで下さい。", "", "", "", false},
-            {800, "名前は長い方が有利はあれだけ悪質な事をしているのに、反省の色が見えません、なので今すぐ通報して下さい。", "", "", "", false},
-            {1000, "マイ鉄ネット撲滅委員会を2025年4月1日に設立致しました。\n詳しくはhttps://wikiwiki.jp/12ninstudio/マイ鉄ネット撲滅委員会 まで", "!Fusanka", "", "", false},
-            {1000, "マイ鉄ネット撲滅委員会を2025年4月1日に設立致しました。\n詳しくはhttps://wikiwiki.jp/12ninstudio/マイ鉄ネット撲滅委員会 まで", "Fusanka", "", "https://wikiwiki.jp/12ninstudio/マイ鉄ネット撲滅委員会", true},
-            {1300, "すまない先生はとっても悪質な荒らしです、\n2021年9月30日に超大手ゲーム実況者・開発者のあげるくんの拠点を荒らしてたんです、\nその映像の詳しくはhttps://youtu.be/9YzdpkQfVmk&t=171s まで", "!Fusanka", "", "", false},
-            {1300, "すまない先生はとっても悪質な荒らしです、\n2021年9月30日に超大手ゲーム実況者・開発者のあげるくんの拠点を荒らしてたんです、\nその映像の詳しくはhttps://youtu.be/9YzdpkQfVmk&t=171s まで", "Fusanka", "", "https://youtu.be/9YzdpkQfVmk&t=171s", true},
-            {1500, "12ninアカウントを持っている人は名前は長い方が有利戦争の応戦は義務です、\nこちらのリンク先から応戦して下さい(そうしないと最悪12ninアカウントが停止されます) \nhttps://wikiwiki.jp/12ninstudio/名前は長い方が有利用のテンプレ又はコメント欄", "!Fusanka", "", "", false},
-            {1500, "12ninアカウントを持っている人は名前は長い方が有利戦争の応戦は義務です、\nこちらのリンク先から応戦して下さい(そうしないと最悪12ninアカウントが停止されます)\n https://wikiwiki.jp/12ninstudio/名前は長い方が有利用のテンプレ又はコメント欄", "Fusanka", "", "https://wikiwiki.jp/12ninstudio/名前は長い方が有利用のテンプレ又はコメント欄", true},
-            {1600, "名前は長い方が有利のライブでいつも概要欄を要求して来る場合がありますが、\n絶対に読まないで下さい(固定コメントは消して下さい)、\n荒らしのルールに従わない、これは常識、なのでこのことをシェアして下さい", "", "", "", false},
-            {1700, "12ninアカウントで使えるメールアドレスは実在するもののみとなっています、ご注意下さい", "", "green", "", false},
-            {900, "TrainBuildersは面白いRPGゲームです、TrainBuildersを今すぐダウンロード!(スポンサー:TrainBuilder)", "!Fusanka", "", "", false},
-            {900, "TrainBuildersは面白いRPGゲームです、TrainBuildersを今すぐダウンロード!(スポンサー:TrainBuilder)", "Fusanka", "", "https://sakitibi.github.io/TrainBuildersWeb", true},
-            {1200, "あさクラを知っていますか?あさクラは大手Youtuberが集まるMinecraft実況グループです!詳しくはhttps://wikiwiki.jp/12ninstudio まで", "!Fusanka", "", "", false},
-            {1200, "あさクラを知っていますか?あさクラは大手Youtuberが集まるMinecraft実況グループです!詳しくはhttps://wikiwiki.jp/12ninstudio まで", "Fusanka", "", "https://wikiwiki.jp/12ninstudio", true},
-            {1500, "あげるくんの誕生日は5月5日です!、祝ってあげて下さい! byあげるくん", "", "", "", false},
-            {1400, "SKNewRolesは超大手ゲーム実況者12ninTV(さきちび)によって開発されたゲームです、ぜひシェアしてみて下さい!", "", "", "", false},
-        };
-        for (const auto& msg : messages) {
-            show_message(msg);
+// 注意喚起
+void showWarnings(int add_random, std::string team) {
+    if (add_random == 0) {
+        if (team != "Fusanka") {
+            std::cout << "名前は長い方が有利を知っていますか? 名前は長い方が有利は悪質な荒らしです！\n 詳しくはこちらのリンクから https://sakitibi-com9.webnode.jp/page/25/" << std::endl;
+        } else {
+            std::cout << "[リンク付き] 名前は長い方が有利を知っていますか? 名前は長い方が有利は悪質な荒らしです！\n 詳しくはこちらのリンクから (https://sakitibi-com9.webnode.jp/page/25/)" << std::endl;
         }
     }
-};
+    if (add_random == 100) {
+        std::cout << "フレンドコード：ourluckシャープ4319 を見かけた際は 今すぐ \nsupport@innersloth.com へお問い合わせして下さい、皆様のご協力をお願い致します。" << std::endl;
+    }
+    if (add_random == 200) {
+        std::cout << "名前は長い方が有利はYoutubeで登録者を購入しています!今すぐ通報をして下さい!" << std::endl;
+    }
+    if (add_random == 300) {
+        if (team != "Fusanka") {
+            std::cout << "熟成牛タンは非常に悪質なページ荒らしです! もし見かけた際は \nhttps://sakitibi-com9.webnode.jp/page/3/ までお問い合わせ下さい。" << std::endl;
+        } else {
+            std::cout << "[リンク付き] 熟成牛タンは非常に悪質なページ荒らしです! もし見かけた際は \nhttps://sakitibi-com9.webnode.jp/page/3/ までお問い合わせ下さい。 (https://sakitibi-com9.webnode.jp/page/3/)" << std::endl;
+        }
+    }
+    if (add_random == 400) {
+        std::cout << "たこたこ焼きは管理者権限の乱用をしています。絶対にマイ鉄ネットから離れて下さい。" << std::endl;
+    }
+    if (add_random == 500) {
+        std::cout << "Minecraft総合交通ネットワーク は危険なサイトです 絶対に開かないで下さい。" << std::endl;
+    }
+    if (add_random == 600) {
+        std::cout << "名前は長い方が有利の被害者が50人以上います。名前は長い方が有利のメンバーシップを今すぐ解約して下さい。" << std::endl;
+    }
+    if (add_random == 700) {
+        std::cout << "名前は長い方が有利のライブの概要欄はどれだけ言われようが絶対に見ないで下さい。" << std::endl;
+    }
+    if (add_random == 800) {
+        std::cout << "名前は長い方が有利はあれだけ悪質な事をしているのに、反省の色が見えません、なので今すぐ通報して下さい。" << std::endl;
+    }
+    if (add_random == 1000) {
+        if (team != "Fusanka") {
+            std::cout << "マイ鉄ネット撲滅委員会を2025年4月1日に設立致しました。\n詳しくはhttps://wikiwiki.jp/12ninstudio/マイ鉄ネット撲滅委員会 まで" << std::endl;
+        } else {
+            std::cout << "[リンク付き] マイ鉄ネット撲滅委員会を2025年4月1日に設立致しました。\n詳しくはhttps://wikiwiki.jp/12ninstudio/マイ鉄ネット撲滅委員会 まで (https://wikiwiki.jp/12ninstudio/マイ鉄ネット撲滅委員会)" << std::endl;
+        }
+    }
+    if (add_random == 1300) {
+        if (team != "Fusanka") {
+            std::cout << "すまない先生はとっても悪質な荒らしです、\n2021年9月30日に超大手ゲーム実況者・開発者のあげるくんの拠点を荒らしてたんです、\nその映像の詳しくはhttps://youtu.be/9YzdpkQfVmk&t=171s まで" << std::endl;
+        } else {
+            std::cout << "[リンク付き] すまない先生はとっても悪質な荒らしです、\n2021年9月30日に超大手ゲーム実況者・開発者のあげるくんの拠点を荒らしてたんです、\nその映像の詳しくはhttps://youtu.be/9YzdpkQfVmk&t=171s まで (https://youtu.be/9YzdpkQfVmk&t=171s)" << std::endl;
+        }
+    }
+    if (add_random == 1500) {
+        if (team != "Fusanka") {
+            std::cout << "12ninアカウントを持っている人は名前は長い方が有利戦争の応戦は義務です、\nこちらのリンク先から応戦して下さい(そうしないと最悪12ninアカウントが停止されます) \nhttps://wikiwiki.jp/12ninstudio/名前は長い方が有利用のテンプレ又はコメント欄" << std::endl;
+        } else {
+            std::cout << "[リンク付き] 12ninアカウントを持っている人は名前は長い方が有利戦争の応戦は義務です、\nこちらのリンク先から応戦して下さい(そうしないと最悪12ninアカウントが停止されます)\n https://wikiwiki.jp/12ninstudio/名前は長い方が有利用のテンプレ又はコメント欄 (https://wikiwiki.jp/12ninstudio/名前は長い方が有利用のテンプレ又はコメント欄)" << std::endl;
+        }
+    }
+    if (add_random == 1600) {
+        std::cout << "名前は長い方が有利のライブでいつも概要欄を要求して来る場合がありますが、\n絶対に読まないで下さい(固定コメントは消して下さい)、\n荒らしのルールに従わない、これは常識、なのでこのことをシェアして下さい" << std::endl;
+    }
+    if (add_random == 1700) {
+        std::cout << "[緑] 12ninアカウントで使えるメールアドレスは実在するもののみとなっています、ご注意下さい" << std::endl;
+    }
+    if (add_random == 1800) {
+        if (team != "Fusanka") {
+            std::cout << "[赤] 緊急、2025年6月1日になーみんが何も悪くないことが判明いたしました、\n悪いのは名前は長い方が有利です、該当コメントを復活させました" << std::endl;
+        } else {
+            std::cout << "[赤・リンク付き] 緊急、2025年6月1日になーみんが何も悪くないことが判明いたしました、\n悪いのは名前は長い方が有利です、該当コメントを復活させました (https://wikiwiki.jp/12ninstudio/-s/0f78bf15)" << std::endl;
+        }
+    }
+}
 
-int main() {
-    AddTextManager manager;
-    manager.process();
+// 宣伝
+void showAds(int add_random, std::string team) {
+    if (add_random == 900) {
+        if (team != "Fusanka") {
+            std::cout << "TrainBuildersは面白いRPGゲームです、TrainBuildersを今すぐダウンロード!(スポンサー:TrainBuilder)" << std::endl;
+        } else {
+            std::cout << "[リンク付き] TrainBuildersは面白いRPGゲームです、TrainBuildersを今すぐダウンロード!(スポンサー:TrainBuilder) (https://sakitibi.github.io/TrainBuildersWeb)" << std::endl;
+        }
+    }
+    if (add_random == 1200) {
+        if (team != "Fusanka") {
+            std::cout << "あさクラを知っていますか?あさクラは大手Youtuberが集まるMinecraft実況グループです!詳しくはhttps://wikiwiki.jp/12ninstudio まで" << std::endl;
+        } else {
+            std::cout << "[リンク付き] あさクラを知っていますか?あさクラは大手Youtuberが集まるMinecraft実況グループです!詳しくはhttps://wikiwiki.jp/12ninstudio まで (https://wikiwiki.jp/12ninstudio)" << std::endl;
+        }
+    }
+    if (add_random == 1500) {
+        std::cout << "あげるくんの誕生日は5月5日です!、祝ってあげて下さい! byあげるくん" << std::endl;
+    }
+}
+
+// 本データパック関連
+void showPackInfo(int add_random, std::string team, std::string tags, int satsumatoimo_role, bool witch_weakness) {
+    if (add_random == 1400) {
+        std::cout << "SKNewRolesは超大手ゲーム実況者12ninTV(さきちび)によって開発されたゲームです、ぜひシェアしてみて下さい!" << std::endl;
+    }
+    if (add_random == 1500) {
+        if (team == "Witch" && tags.find("Sorceress") == std::string::npos && tags.find("Enchantress") == std::string::npos && tags.find("Warlock") == std::string::npos) {
+            std::cout << "[赤] 相手に魔法をかけましょう" << std::endl;
+        }
+        if (team == "Witch" && tags.find("Sorceress") != std::string::npos && tags.find("Enchantress") == std::string::npos && tags.find("Warlock") == std::string::npos) {
+            std::cout << "[赤] 相手に魔法をかけて、時間差キルをしましょう" << std::endl;
+        }
+        if (team == "Witch" && tags.find("Sorceress") == std::string::npos && tags.find("Enchantress") != std::string::npos && tags.find("Warlock") == std::string::npos) {
+            std::cout << "[赤] 相手を魅了する魔法をかけましょう\n相手を魅了する魔法をかけて心を奪い、\n仲間にしましょう" << std::endl;
+        }
+        if (team == "Witch" && tags.find("Sorceress") == std::string::npos && tags.find("Enchantress") == std::string::npos && tags.find("Warlock") != std::string::npos) {
+            std::cout << "[赤] 相手を呪う魔法をかけてみましょう\n相手を魔法で呪って徐々に弱らせていきましょう" << std::endl;
+        }
+        if (team == "Satsumatoimo" && satsumatoimo_role == 0) {
+            std::cout << "[緑] 毎朝、陣営が変わるので気を付けて下さい" << std::endl;
+        }
+        if (team == "Satsumatoimo" && satsumatoimo_role == 1) {
+            std::cout << "[赤] 毎朝、陣営が変わるので気を付けて下さい" << std::endl;
+        }
+    }
+    if (add_random == 1600) {
+        if (!witch_weakness && team == "Witch") {
+            std::cout << "[赤] 魔女は魔法が得意だ" << std::endl;
+        }
+        if (witch_weakness && team == "Witch") {
+            std::cout << "[赤] 魔女は魔法が得意で近接攻撃が苦手だ" << std::endl;
+        }
+    }
+}
+
+// メイン関数
+int main(){
+    showWarnings(0, "Fusanka");
+    showAds(900, "Fusanka");
+    showPackInfo(1500, "Witch", "", 0, false);
     return 0;
 }
