80b242d260436f171ab10c4716b3197e843a73e0
さきちび <sakitibi10nin@icloud.com>
2025-06-02 06:38:16 +0900

    Update remove_first_daytime.cpp

--- Minecraft-Werewolf-Mod-SKNewRoles-main/datapacks/werewolf/data/werewolf/functions/.settings/set_time/set_first_daytime/remove_first_daytime.cpp
+++ Minecraft-Werewolf-Mod-SKNewRoles-main/datapacks/werewolf/data/werewolf/functions/.settings/set_time/set_first_daytime/remove_first_daytime.cpp
@@ -3,12 +3,12 @@
 
 int main() {
     // 該当変数が無い場合初期化
-    if(set_meeting_time === null){
-        public static int set_meeting_time = 0;
+    if(set_first_daytime === null){
+        public static int set_first_daytime = 0;
     }
 
-    if(set_meeting_time_minutes === null){
-        public static int set_meeting_time_minutes = 0;
+    if(set_first_daytime_minutes === null){
+        public static int set_first_daytime_minutes = 0;
     }
 
     if(tick_minute === null){
